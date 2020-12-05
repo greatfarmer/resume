a3f627f7f3d64f2e3e31b1879ae85d26003b4a85
Seonghun Kang <lutis107@gmail.com>
2020-12-06 00:24:57 +0900

    Update contents

--- src/contents.js
+++ src/contents.js
@@ -2,7 +2,7 @@ module.exports = {
   profile: {
     name: '강성훈',
     description: 'Software Developer',
-    email: 'lutis107@gmail.com',
+    email: 'seonghun.dev@gmail.com',
     links: [
       {name: 'github', href: 'https://github.com/greatfarmer'},
       {name: 'linkedin', href: 'https://www.linkedin.com/in/greatfarmer/'}
@@ -10,7 +10,6 @@ module.exports = {
   },
   introduction: `
     ‘뚱이’를 사랑하는 개발자입니다<br />
-    주로 자바와 스프링을 다룹니다<br />
     “세상 사람들이 유용하게 사용할 수 있는 서비스”를 만드는 꿈을 가지고 있습니다<br />
     늦게 개발을 시작한 만큼 열심히 살고 있습니다<br />
     인정받는 것을 좋아합니다(a.k.a. 인정사냥꾼)<br />
@@ -19,17 +18,12 @@ module.exports = {
   contents: {
     '보유기술': [
       {
-        'date': '',
-        'title': '',
-        'contents': ['']
-      },
-      {
         'date': 'Language',
-        'title': 'Java / Python',
+        'title': 'Java / PHP',
       },
       {
         'date': 'Framework',
-        'title': 'Spring',
+        'title': 'Spring / Codeigniter',
       },
       {
         'date': 'Database',
@@ -37,18 +31,28 @@ module.exports = {
       },
       {
         'date': 'ETC',
-        'title': 'Git / Linux / AWS',
+        'title': 'Linux / Git',
       }
     ],
     '프로젝트': [
+      {
+        'date': '2019.08 - 현재',
+        'title': 'FIFArm',
+        'contents': [
+          '- EA SPORTS FIFA ULTIMATE TEAM(FUT) players search site',
+          '- 주요 기능: FUT API SEARCH',
+          '- 사용 기술: Java, Spring Boot, MongoDB',
+          '- <a href="https://github.com/greatfarmer/fifarm">FIFArm github repository</a>'
+        ]
+      },
       {
         'date': '2018.05 - 2018.07',
         'title': '코린이',
         'contents': [
           '- 예비개발자를 위한 프로젝트 관리 사이트',
           '- 주요 기능: 일정, 파일함, 채팅, 관리자페이지',
-          '- 사용 기술: Spring, WebSocket, Firebase Realtime Database',
-          '- <a href="https://github.com/corin2/corin2">corin2 github repository</a>',
+          '- 기술 스택: Java, Spring, MariaDB, Firebase Realtime Database, WebSocket',
+          '- <a href="https://github.com/corin2/corin2">corin2 github repository</a>'
         ]
       },
     ],
@@ -60,20 +64,6 @@ module.exports = {
           '- 코린이 프로젝트를 하며 진행한 AWS EC2, RDS 설정 정리',
           '- AWS 프리티어, 스프링 프레임워크 사용',
           '- <a href="https://www.slideshare.net/SeonghunKang3/aws-ec2-rds">slideshare.net/SeonghunKang3/aws-ec2-rds</a>',
-        ]
-      },
-      {
-        'date': '2019.04',
-        'title': '문자사이에 특정문자를 입력하고 싶을 때',
-        'contents': [
-          '- <a href="https://github.com/greatfarmer/TIL/blob/master/Java/Tip/Java-StringJoiner.md">문자사이에 특정문자를 입력하고 싶을 때</a>',
-        ]
-      },
-      {
-        'date': '2019.04',
-        'title': '실행 가능한 jar파일 만들고 실행하기',
-        'contents': [
-          '- <a href="https://github.com/greatfarmer/TIL/blob/master/Java/Tip/Java-jar.md">실행 가능한 jar파일 만들고 실행하기</a>',
         ]
       },
       {
@@ -95,12 +85,19 @@ module.exports = {
   summary: {
     '경력': [
       {
-        'date': ['2018.09', '2019.12'],
-        'title': 'epopcon',
+        'date': ['2020.08', '현재'],
+        'title': "MUST'IT (머스트잇)",
         'contents': [
-          '개발'
+          'Back End Developer'
         ]
       },
+      {
+        'date': ['2018.09', '2019.12'],
+        'title': 'epopcon (이팝콘)',
+        'contents': [
+          'Developer'
+        ]
+      }
     ]
   },
 };
